47e8a3435aa24fd288ce01e78db6373807344958
gprstersit <gprstersit@gmail.com>
2024-02-09 13:46:05 +0100

    345

--- HW2.py
+++ HW2.py
@@ -14,7 +14,7 @@ def sum_profit(text: str, func: Callable):
     total_sum = sum(func(text))
     return total_sum
 
-# Приклад використання
+
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
