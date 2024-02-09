0334abb18e3d21c8d19194d0e5682a84e4157481
gprstersit <gprstersit@gmail.com>
2024-02-09 13:44:14 +0100

    123

--- HW1.py
+++ HW1.py
@@ -1,29 +1,19 @@
-def caching_fibonacci():
-    # Створюємо порожній словник для кешування
+def caching_fibonacci(): # Створюємо порожній словник для кешування
     cache = {}
-
     def fibonacci(n):
-        # Якщо n <= 0, повертаємо 0
-        if n <= 0:
+        if n <= 0: # Якщо n <= 0, повертаємо 0
             return 0
-        # Якщо n == 1, повертаємо 1
-        elif n == 1:
-            return 1
-        # Якщо n у cache, повертаємо значення з кешу
+        elif n == 1: # Якщо n == 1, повертаємо 1
+            return 1 # Якщо n у cache, повертаємо значення з кешу
         elif n in cache:
             return cache[n]
-        else:
-            # Обчислюємо fibonacci(n) рекурсивно
+        else: # Обчислюємо fibonacci(n) рекурсивно
             result = fibonacci(n - 1) + fibonacci(n - 2)
-            # Зберігаємо результат у кеші
-            cache[n] = result
-            return result
+            cache[n] = result # Зберігаємо результат у кеші
+            return result 
+    return fibonacci # Повертаємо функцію fibonacci
 
-    # Повертаємо функцію fibonacci
-    return fibonacci
-
-# Приклад використання
-fib = caching_fibonacci()
+fib = caching_fibonacci() # Приклад використання
 
 # Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
 print(fib(10))  # Виведе 55
